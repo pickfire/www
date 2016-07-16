d1df42727bfe84dd99b140d8ee0e3faf04649005
Ivan Tham <pickfire@riseup.net>
2016-07-16 16:21:18 +0800

    Minor fix
        - fix date in time machine
        - fix navigation bar

--- posts/index.sh
+++ posts/index.sh
@@ -6,5 +6,5 @@ source config.sh
 for i in $(find ${forig%/*} -name '*.md' -type f|xargs stat -t|awk '{print $13,$1}' \
   |grep -v '_\|index\|.sh$'|sort -r|cut -f2 -d' '|sed 's/\.md//'); do
   LIST+="<a href=\"/${i}\">$(sed -n '/^[A-Z][a-z].*/ p' ${i}.*|head -n1) \
-<span class='date'>$(date -ud `stat -t ${i}.*|cut -f13 -d\ ` +'%a %_d %b %Y')</span></a>
+<span class='date'>$(date -ud `stat -t ${i}.*|cut -f13 -d\ ` +'%a %d %b %Y')</span></a>
 "; done
