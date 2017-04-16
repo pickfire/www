c884ea6906f531098d0c485441614afeae3a36c7
Ivan Tham <pickfire@riseup.net>
2017-04-16 16:08:27 +0800

    Remove 301 redirects on main page

--- _lay/menu.sh
+++ _lay/menu.sh
@@ -2,7 +2,7 @@ LAYOUT=_lay/layout.dhtml
 LINK=${forig%%/*}; LINK=$(tr [a-z] [A-Z] <<< ${LINK:0:1})${LINK:1} # CamelCase
 
 for i in $(grep -sH "^LINK=" */_www/config); do
-  MENU+="<a href=\"/${i%%/*}\""
+  MENU+="<a href=/${i%%/*}/"
   [ ${forig%%/*} = "${i%%/*}" ] && MENU+=" id='here'"
   MENU+=">${i#*LINK=}</a>"
 done
