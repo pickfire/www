6f0507e1a8126bf331578266ae7859cc39d4cf70
Ivan Tham <pickfire@riseup.net>
2019-07-07 01:31:24 +0800

    Fix check.c to use strlcpy and strlcat

--- _dyn/tor/check.c
+++ _dyn/tor/check.c
@@ -7,6 +7,69 @@
 #define TEXT "/var/cache/dyn/exits.txt"
 #define HOST "pickfireywcq2wf2.onion"
 
+/*
+ * Appends src to string dst of size dsize (unlike strncat, dsize is the
+ * full size of dst, not space left).  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
+ * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
+ * If retval >= dsize, truncation occurred.
+ */
+size_t
+strlcat(char *dst, const char *src, size_t dsize)
+{
+        const char *odst = dst;
+        const char *osrc = src;
+        size_t n = dsize;
+        size_t dlen;
+
+        /* Find the end of dst and adjust bytes left but don't go past end. */
+        while (n-- != 0 && *dst != '\0')
+                dst++;
+        dlen = dst - odst;
+        n = dsize - dlen;
+
+        if (n-- == 0)
+                return(dlen + strlen(src));
+        while (*src != '\0') {
+                if (n != 0) {
+                        *dst++ = *src;
+                        n--;
+                }
+                src++;
+        }
+        *dst = '\0';
+
+        return(dlen + (src - osrc));    /* count does not include NUL */
+}
+
+/*
+ * Copy src to string dst of size siz. At most siz-1 characters
+ * will be copied. Always NUL terminates (unless siz == 0).
+ * Returns strlen(src); if retval >= siz, truncation occurred.
+ */
+size_t
+strlcpy(char *dst, const char *src, size_t siz)
+{
+        char *d = dst;
+        const char *s = src;
+        size_t n = siz;
+        /* Copy as many bytes as will fit */
+        if (n != 0) {
+                while (--n != 0) {
+                        if ((*d++ = *s++) == '\0')
+                                break;
+                }
+        }
+        /* Not enough room in dst, add NUL and traverse rest of src */
+        if (n == 0) {
+                if (siz != 0)
+                        *d = '\0'; /* NUL-terminate dst */
+                while (*s++)
+                        ;
+        }
+        return(s - src - 1); /* count does not include NUL */
+}
+
 int main()
 {
 	FILE *fp;
@@ -19,7 +82,8 @@ int main()
 		if (strstr(HOST, getenv("SERVER_NAME")) != NULL)
 			tor = 1;
 		else {
-			ip = strncat(getenv("REMOTE_ADDR"), "\n", 15);
+			strlcpy(ip, getenv("REMOTE_ADDR"), 15);
+			strlcat(ip, "\n", 1);
 
 			if (!(fp = fopen(TEXT, "r")))
 				return 1;
